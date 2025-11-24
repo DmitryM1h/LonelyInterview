fb7598310962427ac13c08fb0bc3d4b5d152d13a
Vernik <163390481+125784@users.noreply.github.com>
2025-11-25 00:27:18 +1200

    Delete RegisterHrDto.cs

--- RegisterHrDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LonelyInterview.Application.Requests.Auth;
-
-public record RegisterHrDto(string UserName, DateOnly? BirthDay, string Company,
-                string Email, string? Telegram, string Password, string Position, string PhoneNumber);
-
-
-public record RegisterHrResponse(string UserName, DateOnly? BirthDay, string Company,
-                string Email, string? Telegram, string Password, string Position, string PhoneNumber);
